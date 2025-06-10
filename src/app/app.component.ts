5118fd03dd233fe52adcc74433f06dccb3004d1c
Alfonso J <alfonsojfp@gmail.com>
2025-06-10 13:50:41 +0200

    Primer commit pec6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatSidenavModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pec6-angular';
+
+  constructor(private router: Router){
+
+  }
 }
